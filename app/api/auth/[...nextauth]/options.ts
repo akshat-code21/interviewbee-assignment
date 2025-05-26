51d879e2d9cff6690921a38643e5de045c4f0ebc
Akshat Sipany <borncancer21@gmail.com>
2025-05-26 22:27:20 +0530

    Add redirect callback to authOptions for improved URL handling during sign-in process

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -25,6 +25,11 @@ export const authOptions: NextAuthOptions = {
         signIn: "/"
     },
     callbacks: {
+        async redirect({ url, baseUrl }) {
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
+        },
         async session({ session, token }: { session: Session, token: JWT }) {
             return {
                 ...session,
